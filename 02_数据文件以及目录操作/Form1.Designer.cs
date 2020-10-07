ca9b955f25baa58c39125f66964e4aa7e4dd6902
俊华 蔡 <1032031912@qq.com>
2020-10-07 11:30:17 +0800

    添加文件操作

---
+++ 02_数据文件以及目录操作/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace _02_数据文件以及目录操作
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox1.ForeColor = System.Drawing.Color.SpringGreen;
+            this.textBox1.Location = new System.Drawing.Point(12, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(316, 446);
+            this.textBox1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(334, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(601, 104);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "日志操作";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(126, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "写入日志";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(392, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 52);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "读取日志";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button10);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox2.Location = new System.Drawing.Point(335, 114);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(600, 176);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "文件操作";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "源文件路径:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 26);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "目的文件路径:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(155, 37);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(439, 33);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(155, 76);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(439, 33);
+            this.textBox3.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(23, 125);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 39);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "删 除 文 件";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(169, 125);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(131, 39);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "复 制 文 件";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(315, 125);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(131, 39);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "移 动 文 件";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button7);
+            this.groupBox3.Controls.Add(this.button9);
+            this.groupBox3.Controls.Add(this.button8);
+            this.groupBox3.Controls.Add(this.button6);
+            this.groupBox3.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox3.Location = new System.Drawing.Point(335, 311);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(600, 138);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "文件目录操作";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(6, 32);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(253, 43);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "显示指定目录下的所有文件";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(265, 32);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(329, 43);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "显示指定目录下的所有子目录";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(6, 81);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(253, 43);
+            this.button8.TabIndex = 0;
+            this.button8.Text = "指定目录下创建一个子目录";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(265, 81);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(329, 43);
+            this.button9.TabIndex = 0;
+            this.button9.Text = "删除指定目录下的所有子目录和文件";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(461, 125);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(131, 39);
+            this.button10.TabIndex = 2;
+            this.button10.Text = "保 存 配 置";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 461);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ 02_数据文件以及目录操作/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using INI_ClassLibrary;
+
+namespace _02_数据文件以及目录操作
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //写入日志
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //创建文件流
+            FileStream fs = new FileStream("D:\\myfile.txt", FileMode.Append);
+            //创建写入器
+            StreamWriter sw = new StreamWriter(fs);
+            //以文件流的形式写入数据
+            sw.WriteLine(DateTime.Now.ToString() + " [Form1] [button1_Click] 写入日志");
+            //关闭写入器
+            sw.Close();
+            //关闭文件流
+            fs.Close();
+        }
+        //读取日志
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //创建文件流
+            FileStream fs = new FileStream("D:\\myfile.txt", FileMode.Open);
+            //创建读取器
+            StreamReader sr = new StreamReader(fs);
+            //以流的方式读取数据
+            this.textBox1.Text = sr.ReadToEnd();
+            //关闭写入器
+            sr.Close();
+            //关闭文件流
+            fs.Close();
+        }
+        //删除
+        private void button3_Click(object sender, EventArgs e)
+        {
+            File.Delete(this.textBox2.Text.Trim());
+        }
+        //复制
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //判断前往的文件夹是否存在同名文件，如果存在删除对应文件
+            if (File.Exists(this.textBox3.Text.Trim()))
+            {
+                File.Delete(this.textBox3.Text.Trim());
+            }
+            //复制新的文件到目标文件夹当中
+            File.Copy(this.textBox2.Text.Trim(), this.textBox3.Text.Trim());
+        }
+        //移动文件
+        private void button5_Click(object sender, EventArgs e)
+        {
+            File.Move(this.textBox2.Text.Trim(), this.textBox3.Text.Trim());
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            //保存绝对路径
+            string Save_File = System.AppDomain.CurrentDomain.BaseDirectory + "日志路径.ini";
+            //保存信息
+            INI.WritePrivateProfileString("日志配置", "源文件路径", this.textBox2.Text, Save_File);
+            INI.WritePrivateProfileString("日志配置", "目的文件路径", this.textBox3.Text, Save_File);
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+        //窗体加载的时候读取INI配置文件
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //INI 的路径
+            string Save_File = System.AppDomain.CurrentDomain.BaseDirectory + "日志路径.ini";
+            //显示读取的内容
+            this.textBox2.Text = INI.ContentValue("日志配置", "源文件路径", Save_File);
+            this.textBox3.Text = INI.ContentValue("日志配置", "目的文件路径", Save_File);
+        }
+        //获取当前目录下的所有文件
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string[] files = Directory.GetFiles("D:\\");
+            this.textBox1.Clear();//清空
+            foreach (string item in files)
+            {
+                this.textBox1.Text += item + "\r\n";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string[] files = Directory.GetDirectories("D:\\");
+            this.textBox1.Clear();//清空
+            foreach (string item in files)
+            {
+                this.textBox1.Text += item + "\r\n";
+            }
+        }
+        //在指定目录下创建子目录
+        private void button8_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                Directory.CreateDirectory("D:\\myfile\\" + i.ToString());
+            }
+        }
+        //删除指定目录
+        private void button9_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo dir = new DirectoryInfo("D:\\myfile");
+            dir.Delete(true);
+        }
+    }
+}
