d426ecd3863a8fc21810b417931ea2127d39f7ce
俊华 蔡 <1032031912@qq.com>
2020-10-08 10:11:25 +0800

    添加ini与xml

---
+++ 03_XML_CSV/Form1.Designer.cs
@@ -0,0 +1,378 @@
+﻿namespace _03_XML_CSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(659, 126);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CSV";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(75, 52);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 59);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "CSV 写入";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(417, 52);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CSV 读取";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox2.Location = new System.Drawing.Point(16, 149);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(315, 315);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "XML 写入与读取";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(159, 249);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(135, 55);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "读 取";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(91, 200);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(201, 39);
+            this.textBox4.TabIndex = 1;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(16, 249);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(135, 55);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "写 入";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(91, 151);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(201, 39);
+            this.textBox3.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(91, 102);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(201, 39);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 200);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 31);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "年龄:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 155);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 31);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "性别:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 106);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 31);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "班级:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(91, 54);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(201, 39);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 58);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "姓名:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.textBox8);
+            this.groupBox3.Controls.Add(this.textBox5);
+            this.groupBox3.Controls.Add(this.textBox7);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.textBox6);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox3.Location = new System.Drawing.Point(369, 149);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Size = new System.Drawing.Size(323, 315);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "序列化反序列化";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(171, 252);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(135, 55);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "反序列化";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(19, 252);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(135, 55);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "序列化";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(95, 204);
+            this.textBox8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(201, 39);
+            this.textBox8.TabIndex = 1;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(95, 54);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(201, 39);
+            this.textBox5.TabIndex = 1;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(95, 155);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(201, 39);
+            this.textBox7.TabIndex = 1;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 204);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 31);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "年龄:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 58);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 31);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "姓名:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(95, 106);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(201, 39);
+            this.textBox6.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 159);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 31);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "性别:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 110);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 31);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "班级:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 494);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ 03_XML_CSV/Form1.cs
@@ -0,0 +1,238 @@
+﻿using CSV_ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace _03_XML_CSV
+{
+    public partial class Form1 : Form
+    {
+        //XML的使用
+        public class XmlConfigUtil
+        {
+            #region 全局变量
+            string _xmlPath;        //文件所在路径
+            #endregion
+
+            #region 构造函数
+            /// <summary>
+            /// 初始化一个配置
+            /// </summary>
+            /// <param name="xmlPath">配置所在路径</param>
+            public XmlConfigUtil(string xmlPath)
+            {
+                _xmlPath = Path.GetFullPath(xmlPath);
+            }
+            #endregion
+
+            #region 公有方法
+            /// <summary>
+            /// 写入配置
+            /// </summary>
+            /// <param name="value">写入的值</param>
+            /// <param name="nodes">节点</param>
+            public void Write(string value, params string[] nodes)
+            {
+                //初始化xml
+                XmlDocument xmlDoc = new XmlDocument();
+                if (File.Exists(_xmlPath))
+                    xmlDoc.Load(_xmlPath);
+                else
+                    xmlDoc.LoadXml("<XmlConfig />");
+                XmlNode xmlRoot = xmlDoc.ChildNodes[0];
+
+                //新增、编辑 节点
+                string xpath = string.Join("/", nodes);
+                XmlNode node = xmlDoc.SelectSingleNode(xpath);
+                if (node == null)    //新增节点
+                {
+                    node = makeXPath(xmlDoc, xmlRoot, xpath);
+                }
+                node.InnerText = value;
+
+                //保存
+                xmlDoc.Save(_xmlPath);
+            }
+
+            /// <summary>
+            /// 读取配置
+            /// </summary>
+            /// <param name="nodes">节点</param>
+            /// <returns></returns>
+            public string Read(params string[] nodes)
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                if (File.Exists(_xmlPath) == false)
+                    return null;
+                else
+                    xmlDoc.Load(_xmlPath);
+
+                string xpath = string.Join("/", nodes);
+                XmlNode node = xmlDoc.SelectSingleNode("/XmlConfig/" + xpath);
+                if (node == null)
+                    return null;
+
+                return node.InnerText;
+            }
+            #endregion
+
+            #region 私有方法
+            //递归根据 xpath 的方式进行创建节点
+            static private XmlNode makeXPath(XmlDocument doc, XmlNode parent, string xpath)
+            {
+
+                // 在XPath抓住下一个节点的名称；父级如果是空的则返回
+                string[] partsOfXPath = xpath.Trim('/').Split('/');
+                string nextNodeInXPath = partsOfXPath.First();
+                if (string.IsNullOrEmpty(nextNodeInXPath))
+                    return parent;
+
+                // 获取或从名称创建节点
+                XmlNode node = parent.SelectSingleNode(nextNodeInXPath);
+                if (node == null)
+                    node = parent.AppendChild(doc.CreateElement(nextNodeInXPath));
+
+                // 加入的阵列作为一个XPath表达式和递归余数
+                string rest = String.Join("/", partsOfXPath.Skip(1).ToArray());
+                return makeXPath(doc, node, rest);
+            }
+            #endregion
+        }
+        //序列化与反序列化：序列化->将内存中的对象(数据)保存成硬盘上的文件
+        //二进制文件         ：反序列化->将硬盘中的文件加载到内存当中
+        [Serializable]
+        public class Student
+        {
+            private string studentName;
+            private string studentClass;
+            private string gender;
+            private string year;
+            public string StudentName { get => studentName; set => studentName = value; }
+           
+            public string Gender { get => gender; set => gender = value; }
+            public string Year { get => year; set => year = value; }
+            public string StudentClass { get => studentClass; set => studentClass = value; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //CSV 写入操作
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //规定写入的路径
+            string path = "学生信息.csv";
+
+            //第一行的内容
+            string[] rowCells_1 = new string[4];
+            //每一个格子的内容写入
+            rowCells_1[0] = "学号";
+            rowCells_1[1] = "姓名";
+            rowCells_1[2] = "班级";
+            rowCells_1[3] = "籍贯";
+
+            //第二行的内容
+            string[] rowCells_2 = new string[4];
+            //每一个格子的内容写入
+            rowCells_2[0] = "001";
+            rowCells_2[1] = "助教";
+            rowCells_2[2] = "语言班";
+            rowCells_2[3] = "东莞";
+
+            //存入
+            List<string[]> rowList_Write = new List<string[]>();
+            //添加每一行的内容
+            rowList_Write.Add(rowCells_1);
+            rowList_Write.Add(rowCells_2);
+
+            //写入CSV当中 最后一个是不追加，覆盖式写入
+            CSV_ClassLibrary.CSV.WriteCSV(path, rowList_Write, false);
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string[]> row_List = CSV.ReadCSV("学生信息.csv");
+            //显示
+            for (int i = 0; i < row_List.Count; i++)
+            {
+                for (int j = 0; j < row_List[i].Length; j++)
+                {
+                    Console.Write(row_List[i][j] + " ");
+                }
+                Console.WriteLine();//换行
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //初始化XML
+            XmlConfigUtil util = new XmlConfigUtil("1.xml");
+            //保存要写入的值以及在XML中的路径
+            util.Write(this.textBox1.Text, "information", "name");
+            util.Write(this.textBox2.Text, "information", "class");
+            util.Write(this.textBox3.Text, "information", "sex");
+            util.Write(this.textBox4.Text, "information", "year");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //初始化XML
+            XmlConfigUtil util = new XmlConfigUtil("1.xml");
+            //保存要写入的值以及在XML中的路径
+            this.textBox1.Text = util.Read("information", "name");
+            this.textBox2.Text = util.Read("information", "class");
+            this.textBox3.Text = util.Read("information", "sex");
+            this.textBox4.Text = util.Read("information", "year");
+        }
+        //序列化
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //实例化对象类
+            Student student = new Student();
+            student.StudentName = this.textBox5.Text;
+            student.StudentClass = this.textBox6.Text;
+            student.Gender = this.textBox7.Text;
+            student.Year = this.textBox8.Text;
+
+            //【1】创建文件流
+            FileStream fs = new FileStream("objStudent.stu", FileMode.Create);
+            //【2】创建二进制格式转换器
+            BinaryFormatter formatter = new BinaryFormatter();
+            //【3】调用序列化方法
+            formatter.Serialize(fs, student);
+            //【4】关闭文件流 
+            fs.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //【1】创建文件流
+            FileStream fs = new FileStream("objStudent.stu", FileMode.Open);
+            //【2】创建二进制格式转换器
+            BinaryFormatter formatter = new BinaryFormatter();
+            //【3】调用反序列化
+            Student objstudent = (Student)formatter.Deserialize(fs);
+            //【4】关闭文件流
+            fs.Close();
+            //赋值到控件上
+            this.textBox5.Text = objstudent.StudentName;
+            this.textBox6.Text = objstudent.StudentClass;
+            this.textBox7.Text = objstudent.Gender;
+            this.textBox8.Text = objstudent.Year;
+
+        }
+    }
+}
